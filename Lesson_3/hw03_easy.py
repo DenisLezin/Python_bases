41a40ef8f06247431f214f49e34d3221a4718328
Denis <santar2000@mail.ru>
2019-01-29 00:08:34 +0300

    Решены задачи easy и normal

--- Lesson_3/hw03_easy.py
+++ Lesson_3/hw03_easy.py
@@ -1,8 +1,21 @@
+
+__author__ = 'Лезин Денис Андреевич'
 # Задание-1:
 # Напишите функцию, округляющую полученное произвольное десятичное число
 # до кол-ва знаков (кол-во знаков передается вторым аргументом).
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
+
+def rnd(num, n):
+    frac_part = num % 1 * (10 ** n)
+    if frac_part % 1 * 10 // 1 > 4:
+        frac_part += 1
+    return (num // 1) + (frac_part // 1 * (10 ** -n))
+
+num = float(input('Input decimal number: '))
+n = int(input('Input number of digits: '))
+
+print(rnd(num, n))
 
 
 # Задание-2:
@@ -10,6 +23,20 @@
 # Решение реализовать в виде функции.
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 # !!!P.S.: функция не должна НИЧЕГО print'ить
+
+def lucky_func(num):
+    def sm_func(num1):
+        sm = 0
+        for elm in num1:
+            sm = sm + int(elm)
+        return sm
+    if sm_func(num[:3]) == sm_func(num[3:]):
+        return 'You are Lucky!'
+    else:
+        return 'Sorry? Not your day...'
+
+num = input('Enter your lucky number: ')
+print(lucky_func(num))
 
 
 
--- Lesson_3/hw03_normal.py
+++ Lesson_3/hw03_normal.py
@@ -1,13 +1,54 @@
+__author__ = 'Лезин Денис Андреевич'
 # Задание-1:
 # Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
 # Первыми элементами ряда считать цифры 1 1
 
+def fib_func(n, m):
+    fib1 = 1
+    fib2 = 1
+    i = 2
+    lst_fib = []
+    while i <= m:
+        fib_sum = fib1 + fib2
+        fib1 = fib2
+        fib2 = fib_sum
+        if i >= n:
+            lst_fib.append(fib1)
+        i += 1
+    return lst_fib
+
+print(fib_func(4, 6))
+
+def fib_func(n, m):
+    lst_fib = []
+    def fib_recur(num):
+        if num == 1 or num ==2:
+            return 1
+        return fib_recur(num - 1) + fib_recur(num - 2)
+    for i in range(n, m + 1):
+        lst_fib.append(fib_recur(i))
+    return lst_fib
+
+n = int(input('Input element to start n = '))
+m = int(input('Input final element m = '))
+print(fib_func(n, m))
 
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
 # Для сортировки используйте любой алгоритм (например пузырьковый).
 # Для решения данной задачи нельзя использовать встроенную функцию и метод sort()
 
+def sort_func(lst):
+    n = 1
+    while n < len(lst):
+        for i in range(len(lst) - n):
+            if lst[i] > lst[i + 1]:
+                lst[i], lst[i + 1] = lst[i + 1], lst[i]
+        n += 1
+    return lst
+
+lst = [272, 54, 255, 479, 591, 830, 900, 147, 287, 63, 695, 579, 490, 262, 218, 121, 481, 732, 123, 590]
+print(sort_func(lst))
 
 #Задача-3:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
