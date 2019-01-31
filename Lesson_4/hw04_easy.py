b602bfcb3df54c85771dfb5ddeb6ed92394aa558
Denis <santar2000@mail.ru>
2019-02-01 02:01:32 +0300

    easy is done

--- Lesson_4/hw04_easy.py
+++ Lesson_4/hw04_easy.py
@@ -1,4 +1,9 @@
+
+__author__ = 'Лезин Денис Андреевичы'
 # Все задачи текущего блока решите с помощью генераторов списков!
+
+import random
+#lst_zero = [random.randint(-10, 40) for _ in range(20)]
 
 # Задание-1:
 # Дан список, заполненный произвольными целыми числами.
@@ -6,12 +11,19 @@
 # квадратами элементов исходного списка
 # [0, 1, 2, 3] --> [0, 1, 4, 9]
 
+lst = [12, 13, 11, 5, 14, 18, 2, 3, 10, 4]
+lst_new = [(i ** 2) for i in lst]
+print(f'Old list = {lst}\nNew list = {lst_new}')
 
 
 # Задание-2:
 # Даны два списка фруктов.
 # Получить список фруктов, присутствующих в обоих исходных списках.
 
+lst_fruits1 = ['apple', 'orange', 'banana', 'kiwi', 'grape']
+lst_fruits2 = ['kiwi', 'plum', 'orange', 'apple']
+lst_fruits3 = [elm for elm in lst_fruits1 if elm in lst_fruits2]
+print(lst_fruits3)
 
 
 # Задание-3:
@@ -21,7 +33,8 @@
 # + Элемент неотрицательный
 # + Элемент не кратен 3
 
-
-
+lst = [random.randint(-10, 40) for _ in range(20)]
+lst_new = [i for i in lst if i % 2 == 0 and i > 0 and not i % 3 == 0]
+print(f'Old list = {lst}\nNew list = {lst_new}')
 
 
