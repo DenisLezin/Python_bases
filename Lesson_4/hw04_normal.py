d72104a14d89a0bf4988df8b9527b4812e4729e5
Denis <santar2000@mail.ru>
2019-02-01 20:50:46 +0300

    easy is done normal is done hard-half

--- Lesson_4/hw04_normal.py
+++ Lesson_4/hw04_normal.py
@@ -1,9 +1,11 @@
+
+__author__ = 'Лезин Денис Андреевич'
 # Задание-1:
 # Вывести символы в нижнем регистре, которые находятся вокруг
 # 1 или более символов в верхнем регистре.
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
-
+'''
 import re
 print(re.findall('[a-z]+', 'mtMmEZUOmcq'))
 
@@ -19,7 +21,7 @@ for i in string:
             ch = ''
 if ch: lst.append(ch) 
 print(lst)
-
+'''
 # Задание-2:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
@@ -28,11 +30,12 @@ print(lst)
 # в вышезаполненном файле.
 
 import random
+import re
 num = ''.join([str(random.randint(0,9)) for _ in range(2500)])
 seq_tmp = ''
 seq = ''
 prev = 10
-
+'''
 for i in num:
     if not seq_tmp or int(i) == prev:
         seq_tmp += i
@@ -43,5 +46,11 @@ for i in num:
 
 with open('test.txt', 'r+') as f:
     f.write(num)
+'''
+# найти самую длинную последовательность через re
+digit_list = re.findall(r'0+|1+|2+|3+|4+|5+|6+|7+|8+|9+', num)
+print(num)
+print(digit_list)
+print(max(digit_list, key=len))
 
 print(seq)
