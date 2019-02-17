911568eee62cd9982ea50f17dca359767096f628
Denis <santar2000@mail.ru>
2019-02-17 23:42:22 +0300

    Easy done

--- Lesson_8/hw08_easy.py
+++ Lesson_8/hw08_easy.py
@@ -5,8 +5,20 @@
 элемента словаря, как к атрибуту класса.
 '''
 
+class DictBuilder:
+    def __init__(self, dict1, dict2):
+        #self.dict1 = dict1
+        #self.dict2 = dict2
+        #self.d = {}
+        i = 0
+        for key in dict1:
+            setattr(self, key, dict2[i])
+            i += 1
 
 
+dict = DictBuilder(['name', 'surname', 'age'], ['Dmitry', 'Lee', 25])
+print(dict.__dict__)
+print(f'{dict.surname} {dict.name}, age {dict.age}')
 
 '''
 Задание 2.
@@ -17,4 +29,15 @@
 списка.
 '''
 
+class ListClearWrapper:
+    def __init__(self, lst):
+        self.wrapped = lst
+
+    def __getattr__(self, attr):
+        return getattr(self.wrapped, attr)
+
+lst = ListClearWrapper(['a', 'f', 34])
+print(lst.wrapped)
+lst.clear()
+print(lst.wrapped)
 
